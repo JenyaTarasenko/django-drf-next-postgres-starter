8e7f0723527c5b9e0e55ce6a35f86d2c565fd878
Kiril <kiril@Mac-jenya.local>
2026-08-20 13:38:46 +0300

    JWT-front-login

--- backend/authentication/authentication.py
+++ backend/authentication/authentication.py
@@ -1,18 +1,36 @@
-# pyrefly: ignore [missing-import]
+# 
+
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-#аунтификация 
+
 class CookieJWTAuthentication(JWTAuthentication):
 
     def authenticate(self, request):
+
+        print("========== JWT DEBUG ==========")
+        print("COOKIES:", request.COOKIES)
+
         access_token = request.COOKIES.get("access_token")
 
+        print("ACCESS TOKEN:", access_token)
+
         if not access_token:
+            print("❌ ACCESS TOKEN NOT FOUND")
+            print("================================")
             return None
+
+        print("✅ ACCESS TOKEN FOUND")
 
         validated_token = self.get_validated_token(access_token)
 
+        print("✅ TOKEN VALID")
+
+        user = self.get_user(validated_token)
+
+        print("✅ USER:", user)
+        print("================================")
+
         return (
-            self.get_user(validated_token),
+            user,
             validated_token,
         )
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,5 +1,4 @@
 
-
 from pathlib import Path
 import os
 from datetime import timedelta
@@ -11,14 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-ig^a#vuppdy5rglgc2g@x=es91t2fu9c+u2m4ydwm86cdyv*s6'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -67,8 +64,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
@@ -88,8 +84,7 @@ DATABASES = {
 # }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -107,9 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -119,13 +111,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -140,13 +129,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
-# ALLOWED_HOSTS = [
-#     # '157.230.143.66',        # прямой IP сервера
-#    
-#     'localhost',             # локальные тесты
-#     '127.0.0.1',
-#     "backend",   # ← ВАЖНО для Docker
-# ]
+
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
